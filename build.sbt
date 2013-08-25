dc67b1f62cbd0b09caa5e0498648063e0182d9c8
Alexey Alekhin <laughedelic@gmail.com>
2013-08-25 18:12:31 +0200

    Rewrote installation proccess, added detailed comments

--- build.sbt
+++ build.sbt
@@ -8,4 +8,4 @@ licenses := Seq("AGPLv3" -> url("http://www.gnu.org/licenses/agpl-3.0.txt"))
 
 bundleObjects := Seq("ohnosequences.statika.Bowtie")
 
-libraryDependencies ++= Seq( "ohnosequences" %% "boost" % "0.1.0-SNAPSHOT" ) 
+libraryDependencies ++= Seq( "ohnosequences" %% "boost" % "0.1.0" ) 
--- src/main/scala/Bowtie.scala
+++ src/main/scala/Bowtie.scala
@@ -2,25 +2,34 @@ package ohnosequences.statika
 
 import ohnosequences.statika._
 import sys.process._
+import java.io.File
+import java.net.URL
 
 case object Bowtie extends Bundle(Boost :: HNil) {
 
   val metadata = generated.metadata.Bowtie
 
   def install[D <: DistributionAux](distribution: D): InstallResults = {
-    val results = List(
-      "mkdir -p /opt/bowtie"
-    , "cd /opt/bowtie"
-    , "wget http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.1.0/bowtie2-2.1.0-linux-x86_64.zip/download -O bowtie2-2.1.0-linux-x86_64.zip"
-    , "unzip bowtie2-2.1.0-linux-x86_64.zip"
-    , "cd bowtie2-2.1.0"
-    , "cp bowtie2 bowtie2-* /usr/bin/"
+    val trace = List[ProcessBuilder](
+      // dowloading archive
+      new URL("http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.1.0/bowtie2-2.1.0-linux-x86_64.zip/download") #> 
+        new File("bowtie2-2.1.0.zip")
+      // extracting archive
+      // (with assumption that it contains bowtie2-2.1.0 folder inside)
+    , Seq("unzip", "bowtie2-2.1.0.zip")
+      // copying binaries to PATH
+      // (this call is made through bash to expand * wildcard)
+    , Seq("sh", "-c", "cp bowtie2-2.1.0/bowtie2* /usr/bin/")
+    // folding this list of commands accumulating results
+    // is something failed, we have the trace of successfull commands before 
+    // and the last which failed
     ).foldLeft(List[InstallResult]()){ (acc, cmd) => 
       if (acc exists (_.isLeft)) acc
-      else if (cmd.! != 0) acc ::: failure(cmd)
-      else acc ::: success(cmd)
+      else if (cmd.! != 0) acc ::: failure(cmd.toString)
+      else acc ::: success(cmd.toString)
     }
-    if (results exists (_.isLeft)) results
+
+    if (trace exists (_.isLeft)) trace
     else success("%s is installed" format metadata)
   }
 
